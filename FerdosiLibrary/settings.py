4b5fbd7ba8ead3bde748883335dce0e99521cbfc
poyan rahmani <poyanr201@gmail.com>
2024-12-02 15:55:18 +0330

    front site asli ro dakhel poshe main bezan

--- FerdosiLibrary/settings.py
+++ FerdosiLibrary/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "library.apps.LibraryConfig"
+    "library.apps.LibraryConfig",
+    "blog.apps.BlogConfig",
+    "main.apps.MainConfig",
+    "user.apps.UserConfig"
 ]
 
 MIDDLEWARE = [
