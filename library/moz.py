b8229d63851a59ec1994cf308fff0a1a2a98eec6
Poyan <147807636+POYANr11@users.noreply.github.com>
2025-01-16 18:34:39 +0330

    Delete library/moz.py

--- library/moz.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from django.contrib.auth.models import User
-
-file = 'students_list.xlsx'
-data = pd.read_excel(file, sheet_name='هفتم')
-for i in data.dropna().values[1:]:
-    User.objects.create_user(username=i[2], password=str(i[1]))
-
